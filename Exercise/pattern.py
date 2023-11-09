46ee142b42440b19995fa89bd582ac1b60777af1
MSWatto <muhammadshoaibwatto@gmail.com>
2023-11-09 20:31:17 +0100

    numpy tiles

--- Exercise/pattern.py
+++ Exercise/pattern.py
@@ -4,6 +4,7 @@ import matplotlib as plt
 class checker:
     def __init__(self,resolution,tile_size):
         black=np.zeros((resolution,resolution))
+        y=np.ones((tile_size,tile_size))
     def draw():
         pass
     def show():
